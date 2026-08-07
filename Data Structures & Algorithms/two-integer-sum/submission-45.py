177905bed5526ae54fd2eefb4c730740a0460463
Caleb Olusola <calebseyi123@gmail.com>
2026-08-07 23:43:07 +0100

    Add: two-integer-sum - submission-45

---
+++ Data Structures & Algorithms/two-integer-sum/submission-45.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # [4,5,6]
+        # target -> 10
+        # initialize an empty dict
+        encountered = {}
+        # iterate through the numbers, check if the [target - num] a.k.a the diff exists in the dict
+        # if it exists, return its the index of the num and the index of the diff's location 
+        # else add the num to the dict with it's index as it's value
+        num, diff = 0, 0
+        for idx, num in enumerate(nums):
+            diff = target - num
+            if diff not in encountered:
+                encountered[num] = idx
+            else:
+                return [encountered[diff], idx]
+        return [0]
