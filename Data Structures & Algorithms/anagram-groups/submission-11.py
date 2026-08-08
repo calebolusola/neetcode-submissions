313df25072311ff0512ee80af7382b3d804551a1
Caleb Olusola <calebseyi123@gmail.com>
2026-08-08 03:43:41 +0100

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.py
@@ -0,0 +1,25 @@
+class Solution:
+    @staticmethod
+    def isAnagramOf(word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        
+        hist = {}
+        for char in word1:
+            hist[char] = hist.get(char, 0) + 1
+        for char in word2:
+            if char not in hist:
+                return False
+            hist[char] -= 1
+        return all(freq == 0 for freq in hist.values())
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for word in strs:
+            # generate a canonical representative for each anagram group
+            # (i.e a sorted string for an anagram will represent every anagram of that group)
+            key = "".join(sorted(word))
+            groups.setdefault(key, []).append(word)
+            
+        return list(groups.values())
