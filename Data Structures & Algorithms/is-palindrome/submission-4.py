af59b13f420d968e904b46fdb6082027d49c886a
Caleb Olusola <calebseyi123@gmail.com>
2026-08-14 22:19:01 +0100

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    @staticmethod
+    def isalnum(c: str):
+        return (
+            ord('A') <= ord(c) <= ord('Z') or
+            ord('a') <= ord(c) <= ord('z') or
+            ord('0') <= ord(c) <= ord('9')
+            )
+    def isPalindrome(self, s: str) -> bool:
+        start = 0
+        s_norm = "".join(filter(self.isalnum, s.lower()))
+        end = len(s_norm)-1
+
+        while start < end:
+            if s_norm[start] != s_norm[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
